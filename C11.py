211119bc2d6ddf7133253b5f9e35d389c39be445
Reshi Raj <rajreshi96@gmail.com>
2024-12-05 10:53:26 -0600

    Finding the missing number in an Array of n-1 integers.

---
+++ C11.py
@@ -0,0 +1,7 @@
+def find_missing_number(arr, n):
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(arr)
+    return expected_sum - actual_sum
+
+
+print(find_missing_number([1, 2, 4, 5, 6], 6))  
